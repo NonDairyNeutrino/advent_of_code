4e33ba5ba873b7fd48b0193589a9f88ec30aa92d
nondairyneutrino <chapmann19@gmail.com>
2023-12-20 20:28:28 -0800

    finish day 9.2

---
+++ 2023/day9/day9.2.jl
@@ -0,0 +1,21 @@
+function parseInput(path :: String)
+    return [(n -> parse(Int, n)).(split(line)) for line in eachline(path)]
+end
+
+function differences(list :: Vector{T}) :: Vector{T} where T <: Number
+    return list[2:end] - list[1:end-1]
+end
+
+function predict(history :: Vector{Int}) :: Int
+    if !allequal(history)
+        return history[1] - predict(differences(history))
+    else
+        return history[1]
+    end
+end
+
+function main()
+    return sum(predict(history) for history in parseInput("input.txt"))
+end
+
+main() |> display
