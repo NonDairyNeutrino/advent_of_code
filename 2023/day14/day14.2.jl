c77d2983f4871e46a1597607d6ea9993e63e5d46
nondairyneutrino <chapmann19@gmail.com>
2023-12-27 22:11:18 -0800

    optimize

--- 2023/day14/day14.2.jl
+++ 2023/day14/day14.2.jl
@@ -1,7 +1,7 @@
 using Test
-using LinearAlgebra
 using Dates
 using Memoization
+using Profile
 
 @enum Direction north east south west
 
@@ -9,7 +9,7 @@ function parseInput(path :: String) :: Matrix{Char}
     return [[line...] for line in eachline(path)] |> stack |> permutedims
 end
 
-function direction(dir :: Direction)
+@memoize function direction(dir :: Direction)
     if dir == north
         return CartesianIndex(-1, 0)
     elseif dir == east
@@ -40,41 +40,62 @@ function isOnNextEdge(platform::Matrix{Char}, tiltDir::Direction, rockPosition::
     end
 end
 
-function findNextCube(platform :: Matrix{Char}, tiltDir :: Direction, rockPosition :: CartesianIndex{2}) :: CartesianIndex{2}
+function findNextCubeCol(platform :: Matrix{Char}, tiltDir :: Direction, rockPosition :: CartesianIndex{2})
     rockRowNumber, rockColNumber = rockPosition |> Tuple
+    colPartial = view(platform, :, rockColNumber)
 
-    if tiltDir == north || tiltDir == south
-        colPartial = platform[:,  rockColNumber]
-
-        if tiltDir == north
-            cubePosition = findprev(==('#'), colPartial, rockRowNumber) # returns nothing if no cube
-            if isnothing(cubePosition)
-                return CartesianIndex(0, rockColNumber)
-            end
-        elseif tiltDir == south
-            cubePosition = findnext(==('#'), colPartial, rockRowNumber) # returns nothing if no cube
-            if isnothing(cubePosition)
-                return CartesianIndex(size(platform)[1] + 1, rockColNumber)
-            end
+    if tiltDir == north
+        cubePosition = findprev(==('#'), colPartial, rockRowNumber) # returns nothing if no cube
+        if isnothing(cubePosition)
+            return CartesianIndex(0, rockColNumber)
         end
-
-        return CartesianIndex(cubePosition, rockColNumber)
-    else
-        rowPartial = platform[rockRowNumber, :]
-
-        if tiltDir == east
-            cubePosition = findnext(==('#'), rowPartial, rockColNumber) # returns nothing if no cube
-            if isnothing(cubePosition)
-                return CartesianIndex(rockRowNumber, size(platform)[2] + 1)
-            end
-        elseif tiltDir == west
-            cubePosition = findprev(==('#'), rowPartial, rockColNumber) # returns nothing if no cube
-            if isnothing(cubePosition)
-                return CartesianIndex(rockRowNumber, 0)
-            end
+    elseif tiltDir == south
+        cubePosition = findnext(==('#'), colPartial, rockRowNumber) # returns nothing if no cube
+        if isnothing(cubePosition)
+            return CartesianIndex(size(platform)[1] + 1, rockColNumber)
         end
-        return CartesianIndex(rockRowNumber, cubePosition)
     end
+
+    return CartesianIndex(cubePosition, rockColNumber)
+end
+
+function findNextCubeRow(platform::Matrix{Char}, tiltDir::Direction, rockPosition::CartesianIndex{2})
+    rockRowNumber, rockColNumber = rockPosition |> Tuple
+    rowPartial = view(platform, rockRowNumber, :)
+
+    if tiltDir == east
+        cubePosition = findnext(==('#'), rowPartial, rockColNumber) # returns nothing if no cube
+        if isnothing(cubePosition)
+            return CartesianIndex(rockRowNumber, size(platform)[2] + 1)
+        end
+    elseif tiltDir == west
+        cubePosition = findprev(==('#'), rowPartial, rockColNumber) # returns nothing if no cube
+        if isnothing(cubePosition)
+            return CartesianIndex(rockRowNumber, 0)
+        end
+    end
+    return CartesianIndex(rockRowNumber, cubePosition)
+end
+
+function findNextCube(platform :: Matrix{Char}, tiltDir :: Direction, rockPosition :: CartesianIndex{2}) :: CartesianIndex{2}
+    if tiltDir == north || tiltDir == south
+        return findNextCubeCol(platform, tiltDir, rockPosition)
+    else
+        return findNextCubeRow(platform, tiltDir, rockPosition)
+    end
+end
+
+function countNextRocksCore(platform::Matrix{Char}, tiltDir::Direction, rockPosition::CartesianIndex{2}, cubePosition::CartesianIndex{2})::Int
+    if !inBounds(platform, cubePosition)
+        cubePosition -= direction(tiltDir)
+    end
+    if rockPosition[1]^2 + rockPosition[2]^2 < cubePosition[1]^2 + cubePosition[2]^2
+        mn, mx = rockPosition, cubePosition
+    else
+        mx, mn = rockPosition, cubePosition
+    end
+    slice = view(platform, mn:mx)
+    return count(==('O'), slice) - 1 # minus one because it counts itself
 end
 
 function countNextRocks(platform :: Matrix{Char}, tiltDir :: Direction, rockPosition :: CartesianIndex{2}) :: Int
@@ -82,18 +103,21 @@ function countNextRocks(platform :: Matrix{Char}, tiltDir :: Direction, rockPosi
         return 0
     else
         cubePosition = findNextCube(platform, tiltDir, rockPosition)
-        if !inBounds(platform, cubePosition)
-            cubePosition -= direction(tiltDir)
-        end
-        mn, mx = sort([rockPosition, cubePosition], by=(ind -> norm(Tuple(ind))))
-        slice  = platform[mn:mx]
-        return count(==('O'), slice) - 1 # minus one because it counts itself
+        return countNextRocksCore(platform, tiltDir, rockPosition, cubePosition)
+    end
+end
+
+function countNextRocks(platform :: Matrix{Char}, tiltDir :: Direction, rockPosition :: CartesianIndex{2}, cubePosition :: CartesianIndex{2}) :: Int
+    if isOnNextEdge(platform, tiltDir, rockPosition)
+        return 0
+    else
+        return countNextRocksCore(platform, tiltDir, rockPosition, cubePosition)
     end
 end
 
 function rollRock(platform :: Matrix{Char}, tiltDir :: Direction, rockPosition :: CartesianIndex{2}) :: CartesianIndex{2}
     nextCubePosition = findNextCube(  platform, tiltDir, rockPosition)
-    newCubeDistance  = countNextRocks(platform, tiltDir, rockPosition) + 1
+    newCubeDistance  = countNextRocks(platform, tiltDir, rockPosition, nextCubePosition) + 1
     rockPositionNew  = nextCubePosition - newCubeDistance * direction(tiltDir)
     return rockPositionNew
 end
@@ -102,24 +126,30 @@ function rollRock(platform :: Matrix{Char}, tiltDir :: Direction)
     return (rockPosition -> rollRock(platform, tiltDir, rockPosition))
 end
 
-function tilt!(platform :: Matrix{Char}, tiltDir :: Direction) :: Matrix{Char}
-    rockPositionVector    = findall(==('O'), platform)
-    rollRockFunction      = rollRock(platform, tiltDir)
-    rockPositionVectorNew = rollRockFunction.(rockPositionVector)
-    platform[rockPositionVector]    .= '.'
-    platform[rockPositionVectorNew] .= 'O'
-    return platform
+function tilt!(platform :: Matrix{Char}, tiltDir :: Direction) :: Tuple{Matrix{Char}, Vector{CartesianIndex{2}}}
+    rockPositionVector            = findall(==('O'), platform)
+    rollRockFunction              = rollRock(copy(platform), tiltDir)
+    platform[rockPositionVector] .= '.'
+    rockPositionVector            = rollRockFunction.(rockPositionVector)
+    platform[rockPositionVector] .= 'O'
+    return platform, rockPositionVector
+end
+
+function tilt!(platform :: Matrix{Char}, tiltDir :: Direction, rockPositionVector :: Vector{CartesianIndex{2}}) :: Tuple{Matrix{Char}, Vector{CartesianIndex{2}}}
+    rollRockFunction              = rollRock(copy(platform), tiltDir)
+    platform[rockPositionVector] .= '.'
+    # println(tiltDir)
+    # println("==============")
+    # display(rockPositionVector)
+    rockPositionVector            .= rollRockFunction.(rockPositionVector)
+    # display(rockPositionVector)
+    # println()
+    platform[rockPositionVector] .= 'O'
+    return platform, rockPositionVector
 end
 
 function tilt(platform :: Matrix{Char}, tiltDir :: Direction) :: Matrix{Char}
     rockPositionVector    = findall(==('O'), platform)
-
-    # find new rock positions in parallel
-
-    #= rockPositionVectorNew = CartesianIndex{2}[]
-    Threads.@threads for rockPosition in rockPositionVector
-        @lock ReentrantLock() push!(rockPositionVectorNew, rollRock(platform, tiltDir, rockPosition))
-    end =#
     rollRockFunction      = rollRock(platform, tiltDir)
     rockPositionVectorNew = rollRockFunction.(rockPositionVector)
     platformNew           = deepcopy(platform)
@@ -128,31 +158,40 @@ function tilt(platform :: Matrix{Char}, tiltDir :: Direction) :: Matrix{Char}
     return platformNew
 end
 
-function cycle!(platform :: Matrix{Char}) :: Matrix{Char}
+#= function cycle!(platform :: Matrix{Char}) :: Matrix{Char}
+    rockPositionVector = findall(==('O'), platform)
     for tiltDir in [north, west, south, east]
-        tilt!(platform, tiltDir)
+        tilt!(platform, tiltDir, rockPositionVector)
+    end
+    return platform
+end =#
+
+function cycle!(platform :: Matrix{Char}, rockPositionVector :: Vector{CartesianIndex{2}}) :: Matrix{Char}
+    for tiltDir in [north, west, south, east]
+        tilt!(platform, tiltDir, rockPositionVector)
     end
     return platform
 end
 
-function cycle!(platform :: Matrix{Char}, n :: Int) :: Matrix{Char}
-    # start = now()
-    # t0 = start
+function cycle!(platform :: Matrix{Char}, n :: Int; progress=false) :: Matrix{Char}
+    start = now()
+    t0 = start
+    rockPositionVector = findall(==('O'), platform)
     for i in 1:n
-        cycle!(platform)
+        cycle!(platform, rockPositionVector)
 
-        # if iszero(i % (n/10^4))
-        #     t = now()
-        #     # println("Current time: $t, Previous Time: $t0, Difference: $((t - t0).value / 1000) seconds")
-        #     # dt               = (t - t0).value / 1000 # seconds / 10^5 steps
-        #     percentCompleted = 100 * i / n
-        #     stepsLeft        = n - i
-        #     # timeRemaining    = dt * 10^5 * stepsLeft
-        #     timeElapsed      = round((t - start).value / 1000 / 60, sigdigits = 2) # minutes
+        reportPercent = 0.01
+        reportSteps   = (reportPercent / 100) * n
+        if progress && iszero(i % reportSteps)
+            t = now()
+            percentCompleted = 100 * i / n
+            stepsRemaining   = n - i
+            timeRemaining    = round(Int64, (((t - t0).value / reportSteps #= average milliseconds per report =#) * stepsRemaining) #= milliseconds =# / 1000 / 60) |> Minute # minutes left
+            timeElapsed      = round(Int64, (t - start).value / 1000 / 60) |> Minute # minutes
 
-        #     println("$percentCompleted% completed; Elapsed time: $timeElapsed minutes") #; Estimated Remaining Time: $timeRemaining")
-        #     t0 = now()
-        # end
+            println("$percentCompleted% completed; Elapsed time: $timeElapsed; Estimated Remaining Time: $timeRemaining")
+            t0 = now()
+        end
     end
     return platform
 end
@@ -165,13 +204,13 @@ function cycle(platform :: Matrix{Char}) :: Matrix{Char}
     return platformNew
 end
 
-function cycle(platform :: Matrix{Char}, n :: Int) :: Matrix{Char}
+function cycle(platform :: Matrix{Char}, n :: Int; progress=false :: Bool) :: Matrix{Char}
     platformNew = deepcopy(platform)
     start = now()
     t0 = start
     for i in 1:n
         platformNew = cycle(platformNew)
-        if iszero(i % (n/10^4))
+        if progress && iszero(i % (n/10^4))
             t = now()
             # println("Current time: $t, Previous Time: $t0, Difference: $((t - t0).value / 1000) seconds")
             # dt               = (t - t0).value / 1000 # seconds / 10^5 steps
@@ -193,11 +232,11 @@ end
 
 echo(x) = (display(x); x)
 function testSuite()
-    @testset verbose = true begin #= for rockPosition in rockPositionVector =#
-        platform  = parseInput("inputTest.txt")
-        platform0 = deepcopy(platform)
+    @testset verbose = true failfast = true begin #= for rockPosition in rockPositionVector =#
+        platform0          = parseInput("inputTest.txt")
+        platform           = deepcopy(platform0)
         rockPositionVector = findall(==('O'), platform)
-        rockPosition = rockPositionVector[10]
+        rockPosition       = rockPositionVector[10]
 
         println("Testing for rock at $rockPosition")
         @testset "isOnNextEdge" begin
@@ -228,12 +267,15 @@ function testSuite()
             platformFinal = parseInput("outputTest.txt")
             @test tilt(platform, north)     == platformFinal
             platformNew = deepcopy(platform)
-            @test tilt!(platformNew, north) == platformFinal
+            @test tilt!(platformNew, north)[1] == platformFinal
         end
         @testset "cycle $n" for n in 1:3 
             platformCycle = parseInput("inputTestCycle$n.txt")
-            @test cycle(platform, n)  == platformCycle
-            @test cycle!(platform, n) == platformCycle
+            @test cycle(platform, n) == platformCycle
+            # println("input")
+            # display(platform)
+            # println("calculated output")
+            @test #= echo( =#cycle!(platform, n)#= ) =# == (#= println("test output"); echo(=#platformCycle)#= ) =#
             platform = deepcopy(platform0)
         end
     end
@@ -246,11 +288,13 @@ end
 4. for each rock, move to site n_r + 1 in opposite direction of rock
 =#
 function main()
-    platform = parseInput("inputTest.txt")
-    platformNew = cycle!(platform, 10^9)
+    platform = parseInput("input.txt")
+    platformNew = cycle!(platform, 10^9, progress = true)
     rockPositionVectorNew = findall(==('O'), platformNew)
     return sum(calculateLoad(platformNew, rockPositionNew) for rockPositionNew in rockPositionVectorNew)
 end
 
 testSuite()
-# main() |> display
+# platform = parseInput("inputTest.txt")
+# @profview cycle!(platform, 10^5, progress=false)
+main() |> display
