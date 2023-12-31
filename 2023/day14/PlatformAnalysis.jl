c6b142a4f3c89acb86724cd17f24b31449aa8993
NonDairyNeutrino <nathaniel.chapman@cwu.edu>
2023-12-31 10:57:24 -0800

    fix findBoundary

--- 2023/day14/PlatformAnalysis.jl
+++ 2023/day14/PlatformAnalysis.jl
@@ -1,5 +1,16 @@
 using Test
 
+struct Cube
+    position  :: CartesianIndex{2}
+    cubeNorth :: Cube
+    cubeSouth :: Cube
+    cubeEast  :: Cube
+    cubeWest  :: Cube
+    # function Cube()
+
+    # end
+end
+
 struct Platform
     diagram      :: Matrix{Char}
     boundary     :: Vector{Cube}
@@ -14,35 +25,24 @@ struct Platform
     end
 end
 
-struct Cube
-    position  :: CartesianIndex{2}
-    cubeNorth :: Cube
-    cubeSouth :: Cube
-    cubeEast  :: Cube
-    cubeWest  :: Cube
-    # function Cube()
-
-    # end
-end
-
 function parseInput(path :: String) :: Matrix{Char}
     return [[line...] for line in eachline(path)] |> stack |> permutedims
 end
 
 function findBoundary(platformDiagram :: Matrix{Char}) :: Vector{CartesianIndex{2}}
     numRows, numCols = size(platformDiagram)
-    boundaryNorth   = CartesianIndices((0:0, 1:numCols))
-    boundaryWest    = CartesianIndices((1:numRows, 0:0))
-    boundarySouth   = CartesianIndices((numRows + 1, numCols))
-    boundaryEast    = CartesianIndices((numRows, numCols + 1))
-    return sort(union(boundaryNorth, boundaryWest, boundarySouth, boundaryEast))
+    boundaryNorth   = CartesianIndices((0:0, 1 : numCols))
+    boundaryWest    = CartesianIndices((1 : numRows, 0:0))
+    boundarySouth   = CartesianIndices(((i -> i : i)(numRows + 1), 1 : numCols))
+    boundaryEast    = CartesianIndices((1 : numRows, (i -> i : i)(numCols + 1)))
+    return union(boundaryNorth, boundaryWest, boundarySouth, boundaryEast)
 end
 
-function findInternalCubes(platformDiagram :: Matrix{Char}) :: Set{CartesianIndex{2}}
-    return findall(==('#'), platformDiagram) |> Set
+function findInternalCubes(platformDiagram :: Matrix{Char}) :: Vector{CartesianIndex{2}}
+    return findall(==('#'), platformDiagram) # manifestly ordered by column
 end
 
-function findCubes(platformDiagram :: Matrix{Char}) :: Set{CartesianIndex{2}}
+function findCubes(platformDiagram :: Matrix{Char}) :: Vector{CartesianIndex{2}}
     return union(findBoundary(platformDiagram), findInternalCubes(platformDiagram))
 end
 
@@ -54,7 +54,12 @@ function testSuite()
     @testset verbose=true begin
         platform = parseInput("inputTest.txt")
         @testset "Cubes" begin
-            boundaryManual      = union(CartesianIndices((0:0, 1:10)), CartesianIndices((1:10, 0:0)), CartesianIndices((11, 10)), CartesianIndices((10, 11)))
+            boundaryManual      = union(
+                CartesianIndices((0:0, 1:10)),   # north boundary
+                CartesianIndices((1:10, 0:0)),   # west boundary
+                CartesianIndices((11:11, 1:10)), # south boundary
+                CartesianIndices((1:10, 11:11))  # east boundary
+            )
             internalCubesManual = Set([CartesianIndex(1, 6), CartesianIndex(2, 5), CartesianIndex(2, 10), CartesianIndex(3, 6), CartesianIndex(3, 7), CartesianIndex(4, 4), CartesianIndex(5, 9), CartesianIndex(6, 3), CartesianIndex(6, 8), CartesianIndex(6, 10), CartesianIndex(7, 6), CartesianIndex(9, 1), CartesianIndex(9, 6), CartesianIndex(9, 7), CartesianIndex(9, 8), CartesianIndex(10, 1), CartesianIndex(10, 6)])
             @test findBoundary(platform)      == boundaryManual
             @test findInternalCubes(platform) == internalCubesManual
