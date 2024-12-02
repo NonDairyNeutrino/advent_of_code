4e887bff495c343b0ee4c2ca699f86d97f651242
nondairyneutrino <chapmann19@gmail.com>
2024-12-02 08:45:07 -0800

    begin d02p2

--- 2024/src/day_02/part_2/d02p2.jl
+++ 2024/src/day_02/part_2/d02p2.jl
@@ -1,7 +1,33 @@
 # Day 02, Part 2
 #=
 Plan of Attack:
-1. Step one description.
-2. Step two description.
-3. Additional steps...
+1. import data as matrix of ints
+2. get test each row if safe
 =#
+
+const RECORDVECTOR = readlines("input.txt") .|> (line -> parse.(Int, split(line)))
+const CHANGEMIN = 1
+const CHANGEMAX = 3
+const FAULTTOLERANCE = 1
+
+function isSafe!(record :: Vector{Int}, faultCount = 0) :: Bool
+    # base case
+    (faultCount == FAULTTOLERANCE + 1 || isempty(record)) && return false
+    # determine if it SHOULD be increasing or decreasing
+    direction = record[1] < record[2] ? isless : Base.isgreater
+
+    for i in 1:length(record)-1
+        left, right = record[i:i+1]
+        if !direction(left, right)
+            faultCount += 1
+            return isSafe!(deleteat!(record, i+1), faultCount)
+        end
+        if !(CHANGEMIN <= abs(left - right) <= CHANGEMAX)
+            faultCount += 1
+            return isSafe!(deleteat!(record, i+1), faultCount)
+        end
+    end
+    return true
+end
+
+count(record -> (!isSafe!(record) && println("record: $record "); isSafe!(record)), RECORDVECTOR)
