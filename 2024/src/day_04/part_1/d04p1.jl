0b8eb02f0cdfc23c96ffda7e9a489bf659eb45ad
nondairyneutrino <chapmann19@gmail.com>
2024-12-01 16:57:17 -0800

    create full directory tree

---
+++ 2024/src/day_04/part_1/d04p1.jl
@@ -0,0 +1,7 @@
+# Day 04, Part 1
+#=
+Plan of Attack:
+1. Step one description.
+2. Step two description.
+3. Additional steps...
+=#
---
+++ 2024/src/day_15/part_2/d15p2.jl
@@ -0,0 +1,7 @@
+# Day 15, Part 2
+#=
+Plan of Attack:
+1. Step one description.
+2. Step two description.
+3. Additional steps...
+=#
