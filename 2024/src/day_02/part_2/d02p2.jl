4c0b3c228f1f67a8976df2edd1cb9c6b6b00df32
nondairyneutrino <chapmann19@gmail.com>
2024-12-03 01:01:49 -0800

    update d02p2

--- 2024/src/day_02/part_2/d02p2.jl
+++ 2024/src/day_02/part_2/d02p2.jl
@@ -11,22 +11,47 @@ const CHANGEMIN = 1
 const CHANGEMAX = 3
 const FAULTTOLERANCE = 1
 
-function isMonotonic(record :: Vector{Int}) :: Bool
+"""
+    isMonotonic(record :: Vector{Int}) :: Bool
+
+Determines if a record is monotonic.
+"""
+function isMonotonic(record :: Vector{Int}) :: Tuple{Bool, Int}
     direction = record[1] < record[2] ? isless : Base.isgreater
     for i in 1:length(record)-1
         left, right = record[i:i+1]
-        !direction(left, right) && return false
+        !direction(left, right) && return (false, i+1)
     end
-    return true
+    return (true, -1)
 end
 
 # isMonotonic test
 @testset "isMonotonic testing" begin
-@test isMonotonic([1,2,3,4]) == true
-@test isMonotonic([4,3,2,1]) == true
-@test isMonotonic([1,2,2,1]) == false
-@test isMonotonic([1,1,1,1]) == false
-@test isMonotonic([1,2,3,2,1]) == false
+    @test isMonotonic([1,2,3,4]) == (true, -1)
+    @test isMonotonic([4,3,2,1]) == (true, -1)
+    @test isMonotonic([1,2,2,1]) == (false, 3)
+    @test isMonotonic([1,1,1,1]) == (false, 2)
+    @test isMonotonic([1,2,3,2,1]) == (false, 4)
+end
+
+"""
+    isSlow(record :: Vector{Int}) :: Bool
+
+Determines if a record changes slow enough.
+"""
+function isSlow(record :: Vector{Int}) :: Tuple{Bool, Int}
+    for i in 1:length(record)-1
+        left, right = record[i:i+1]
+        !(CHANGEMIN <= abs(left - right) <= CHANGEMAX) && return (false, i + 1)
+    end
+    return (true, -1)
+end
+
+@testset "isSlow Testing" begin
+    @test isSlow([1,2,3,4]) == (true, -1)
+    @test isSlow([4,3,2,1]) == (true, -1)
+    @test isSlow([1,2,3,7]) == (false, 4)
+    @test isSlow([0,5,10,15]) == (false, 2)
 end
 
 """
@@ -42,18 +67,19 @@ function isSafe!(record :: Vector{Int}, faultCount = 0) :: Bool
     # determine if the record SHOULD be increasing or decreasing
     direction = record[1] < record[2] ? isless : Base.isgreater
 
-    for i in 1:length(record)-1
-        left, right = record[i:i+1]
-        if !direction(left, right)
-            faultCount += 1
-            return isSafe!(deleteat!(record, i+1), faultCount)
-        end
-        if !(CHANGEMIN <= abs(left - right) <= CHANGEMAX)
-            faultCount += 1
-            return isSafe!(deleteat!(record, i+1), faultCount)
-        end
-    end
-    return true
+    # for i in 1:length(record)-1
+    #     left, right = record[i:i+1]
+    #     if !direction(left, right)
+    #         faultCount += 1
+    #         return isSafe!(deleteat!(record, i+1), faultCount)
+    #     end
+    #     if !(CHANGEMIN <= abs(left - right) <= CHANGEMAX)
+    #         faultCount += 1
+    #         return isSafe!(deleteat!(record, i+1), faultCount)
+    #     end
+    # end
+    # return true
+    !isMonotonic(record) && return isSafe!(deleteat!(record, i+1), faultCount)
 end
 
 # filter(!isSafe!, RECORDVECTOR)
