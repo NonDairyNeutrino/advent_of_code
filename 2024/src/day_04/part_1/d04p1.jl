6b6e0f3696a1edd3676427ff7e924cef31c8a83a
nondairyneutrino <chapmann19@gmail.com>
2024-12-04 09:48:16 -0800

    start d04p1

--- 2024/src/day_04/part_1/d04p1.jl
+++ 2024/src/day_04/part_1/d04p1.jl
@@ -1,7 +1,69 @@
 # Day 04, Part 1
 #=
 Plan of Attack:
-1. Step one description.
-2. Step two description.
-3. Additional steps...
+1. 
 =#
+using Test
+test = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+const east  = CartesianIndex(1,0)
+const ne    = CartesianIndex(1,-1)
+const north = CartesianIndex(0, -1)
+const nw    = CartesianIndex(-1,-1)
+const west  = CartesianIndex(-1,0)
+const sw    = CartesianIndex(-1,1)
+const south = CartesianIndex(0, 1)
+const se    = CartesianIndex(1,1)
+
+grid :: Matrix{Char} = readlines("input.txt") .|> collect |> stack
+
+function getLetter(grid :: Matrix{Char}, position :: CartesianIndex, length :: Int, direction :: CartesianIndex) :: Char
+    displacement :: CartesianIndex  = length * direction
+    positionFinal :: CartesianIndex = position + displacement
+    try
+        return grid[positionFinal]
+    catch e
+        println("Edge detected; Returning 'E'")
+        return 'E'
+    end
+end
+
+mat = ['a' 'b' 'c'; 'd' 'e' 'f'; 'g' 'h' 'i']
+display(mat)
+center = CartesianIndex(2,2)
+length = 1
+@testset "getLetter" begin
+    dirVector = [east, ne, north, nw, west, sw, south, se]
+    @testset "Unit Direction: $dir" for (dir, res) in zip(dirVector, ['f', 'c', 'b', 'a', 'd', 'g', 'h', 'i'])
+        @test getLetter(mat, center, length, dir) == res
+    end
+end
+
+# function getWord(grid :: Matrix{Char}, position :: CartesianIndex, length :: Int, direction :: CartesianIndex)
+#     for r in 1:length
+#         index :: CartesianIndex = position + r * direction
+#         grid[]
+# end
+
+# posVector :: Vector{CartesianIndex{2}} = findall(c -> c == 'X', grid)
+# posVector .+ CartesianIndex()
+
+# function getDirections(index :: CartesianIndex) :: Vector{String}
+#     getEast()
+#     getNE()
+#     getNorth()
+#     getNW()
+#     getWest()
+#     getSW()
+#     getSouth()
+#     getSE()
+# end
