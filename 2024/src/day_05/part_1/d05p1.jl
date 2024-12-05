93d5a63b7a7127cdad6d54756ba873664c10a118
nondairyneutrino <chapmann19@gmail.com>
2024-12-04 22:29:37 -0800

    finish d05p1

--- 2024/src/day_05/part_1/d05p1.jl
+++ 2024/src/day_05/part_1/d05p1.jl
@@ -1,7 +1,38 @@
 # Day 05, Part 1
 #=
 Plan of Attack:
-1. Step one description.
-2. Step two description.
-3. Additional steps...
+1. find all valid updates
+    1.1 filter rules based on update members
+    1.2 test update for each filtered rule
+2. find middle page number
+3. add all middle page numbers
 =#
+
+updateVector :: Vector{Vector{Int}} = readlines("updates.txt") .|> (line -> split(line, ",")) .|> (update -> parse.(Int, update))
+ruleVector   :: Vector{Vector{Int}} = readlines("rules.txt") .|> (line -> split(line, "|")) .|> (rule -> parse.(Int, rule))
+
+function filterRules(update :: Vector{Int}) :: Vector{Vector{Int}}
+    return filter(issubset(update), ruleVector)
+end
+
+function isInOrder(update :: Vector{Int}, rule :: Vector{Int}) :: Bool
+    return intersect(update, rule) == rule
+end
+
+function getCenter(update :: Vector{Int}) :: Int
+    return update[cld(length(update), 2)]
+end
+
+function main()
+    total = 0
+    for update in updateVector
+        # println(ruleVector)
+        filteredRules :: Vector{Vector{Int}} = filterRules(update)
+        if all(rule -> isInOrder(update, rule), filteredRules)
+            total += getCenter(update)
+        end
+    end
+    return total
+end
+
+main()
