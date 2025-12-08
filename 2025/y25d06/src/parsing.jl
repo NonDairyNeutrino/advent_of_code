2d0a092608cef30f7f1cfbee31cd1707aca16dd7
nondairyneutrino <chapmann19@gmail.com>
2025-12-07 20:25:11 -0800

    start y25d06p2

--- 2025/y25d06/src/parsing.jl
+++ 2025/y25d06/src/parsing.jl
@@ -1,12 +1,19 @@
-function parse_input(input :: AbstractString) :: Vector{Problem}
-    argnv..., opv = split(input, '\n')
-
+function parse_args(argnv)
     argm          = split.(argnv) |> stack # [arg1prob1 arg1prob2, ...; arg2prob1, arg2prob2, ...; ...]
     argm          = parse.(Int, argm)
+    return argm
+end
 
+function parse_ops(opv)
     opv           = split(opv)
     opv           = Symbol.(opv)
+    return opv
+end
 
-    probs         = Problem.(opv, eachrow(argm))
+function parse_input(input :: AbstractString) :: Vector{Problem}
+    argnv..., opv = split(input, '\n')
+    argm  = parse_args(argnv)
+    opv   = parse_ops(opv)
+    probs = Problem.(opv, eachrow(argm))
     return probs
 end
