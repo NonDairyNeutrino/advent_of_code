a07c42299a9c37821b483fb6714fde25f993016c
nondairyneutrino <chapmann19@gmail.com>
2025-12-14 15:33:16 -0800

    begin y25d01p2

--- 2025/y25d01/src/Dial.jl
+++ 2025/y25d01/src/Dial.jl
@@ -1,18 +1,22 @@
 mutable struct Dial{T}
     bound :: T
     loc   :: T
-    function Dial{T}(bound :: T, loc :: T) where T <: Integer
+    passes :: T
+    function Dial{T}(bound :: T, loc :: T, passes :: T) where T <: Integer
         @assert 0 <= loc < bound
-        return new{T}(bound, loc)
+        return new{T}(bound, loc, passes)
     end
 end
+Dial{T}(bound, loc) where T = Dial{T}(bound, loc, zero(T))
 
-Base.show(io :: IO, dial :: Dial) = print(io, "0<", dial.loc, "<", dial.bound)
+Base.show(io :: IO, dial :: Dial) = print(io, "0<", dial.passes, "+", dial.loc, "<", dial.bound)
 
 function spin!(dial :: Dial{T}, move :: T) :: Dial{T} where T <: Integer
-    spun_loc = mod(dial.loc + move, dial.bound)
+    spins_from_zero, spun_loc = divrem(dial.loc + move, dial.bound, RoundDown)
+    nspins = spins_from_zero - dial.passes
     @debug "Spin State" dial move spun_loc
     dial.loc = spun_loc
+    dial.passes += abs(nspins) + iszero(spun_loc)
     return dial
 end
 
--- 2025/y25d01/src/y25d01.jl
+++ 2025/y25d01/src/y25d01.jl
@@ -10,14 +10,20 @@ function main1(input)
     dial = Dial{Int}(bound, start)
     movev = parse_input(input)
 
-    dial_seq  = accumulate((dial, move) -> spin(dial, move), movev; init = dial)
+    dial_seq = accumulate((dial, move) -> spin(dial, move), movev; init = dial)
     nzeros = count(dial -> iszero(dial.loc), dial_seq)
     @debug "" dial_seq
     return nzeros
 end
 
 function main2(input)
+    bound = 100
+    start = 50
+    dial = Dial{Int}(bound, start)
+    movev = parse_input(input)
 
+    foreach(move -> spin!(dial, move), movev)
+    return dial.passes
 end
 
 function main()
@@ -27,6 +33,10 @@ function main()
     p1e     = main1(example)
     p1      = main1(input)
     @info "Part 1" p1e p1
+
+    p2e     = main2(example)
+    p2      = main2(input)
+    @info "Part 2" p2e p2
 end
 
 end # module y25d01
