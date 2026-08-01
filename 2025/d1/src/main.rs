d718c59ea24fd08ce5d02fb5ecea921b3e6ea754
Natalie Chapman <massivephonon.proton.me>
2026-08-01 13:47:07 -0700

    parse into dir-len pairs

--- 2025/d1/src/main.rs
+++ 2025/d1/src/main.rs
@@ -1,27 +1,40 @@
-use std::{env::SplitPaths, fs::read_to_string};
-use std::io::Result;
+use std::fmt::{self, Display};
+use std::fs::read_to_string;
+// use std::io::Result;
 use std::str::{FromStr, Split};
 
 struct Rotation {
-    dir: char,
-    len: u8
+    dir: i8,
+    len: u8,
+}
+impl Display for Rotation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.dir, self.len)
+    }
 }
 
-fn parse_input(path: &str) -> impl Iterator<Item = Rotation> { // add return type
-    let input_res: Result<String> = read_to_string(path); // result wrapping file contents
-    let rots: Split<'_, char> = input_res.unwrap().split('\n');            // iterator over rotations
-    rots.map(
-        | rot | -> Rotation {
+fn parse_input(path: &str) -> Vec<Rotation> {
+    let input_str: String = read_to_string(path).unwrap();
+    let input_split: Split<'_, char> = input_str.split('\n');
+    let rot_vec: Vec<Rotation> = input_split
+        .map(|rot| -> Rotation {
             let (dir_str, len_str) = rot.split_at(1);
-            let dir: char = dir_str.chars().next().unwrap();
-            let len: u8   = u8::from_str(len_str).unwrap();
-            Rotation { dir: dir, len: len}
-        }
-    )
+            let dir: i8 = match dir_str.chars().next().unwrap() {
+                'L' => -1,
+                'R' => 1,
+                _ => 0,
+            };
+            let len: u8 = u8::from_str(len_str).unwrap_or_default();
+            Rotation { dir: dir, len: len }
+        })
+        .collect();
+    rot_vec
 }
 
 fn main() {
     const START: u8 = 50;
-    const INPUT_PATH: &str = "input.txt";
-    let init: parse_input(INPUT_PATH)
+    println!("Starting at {}", START);
+    const INPUT_PATH: &str = "input_test.txt";
+    let rot_vec: Vec<Rotation> = parse_input(INPUT_PATH);
+    println!("Rotations parsed; First rotation is {}", rot_vec[0]);
 }
