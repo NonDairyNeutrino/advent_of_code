197c5041b097e5fee026458550256e73868d9ac8
Natalie Chapman <massivephonon.proton.me>
2026-08-02 09:46:14 -0700

    actually get ints out

--- 2025/d1/src/main.rs
+++ 2025/d1/src/main.rs
@@ -2,12 +2,12 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
 use std::path::Path;
 
-fn parse_input(
-    path: &Path,
-) -> std::iter::Map<Lines<BufReader<File>>, impl FnMut(Result<String, std::io::Error>) -> String> {
+fn parse_input(path: &Path) -> impl Iterator<Item = i8> {
     let file: File = File::open(path).unwrap(); // open the file
     let lines: Lines<BufReader<File>> = BufReader::new(file).lines(); // iterator to the reader of the lines of the file
-    lines.map(|line| -> String { line.unwrap().replace("L", "-").replace("R", "") })
+    // iterator over the lines but with L replaced with - and R replaced with nothing to be positive
+    let rot_strs = lines.map(|line| -> String { line.unwrap().replace("L", "-").replace("R", "") });
+    rot_strs.map(|rot_str| -> i8 { rot_str.parse::<i8>().unwrap() })
 }
 
 fn main() {
@@ -19,6 +19,6 @@ fn main() {
     // if accumulator is zero, incrememnt counter
     // repeat
     let path: &Path = Path::new("input_test.txt");
-    let lines = parse_input(path);
-    lines.for_each(|line| println!("Line: {}", line));
+    let roterator = parse_input(path);
+    roterator.for_each(|rot| println!("Line: {}", rot));
 }
