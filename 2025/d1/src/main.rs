0ccfb1a9da06a185ad0d6bb49f74997015182377
Natalie Chapman <massivephonon.proton.me>
2026-08-01 19:25:18 -0700

    replace direction with sign

--- 2025/d1/src/main.rs
+++ 2025/d1/src/main.rs
@@ -1,9 +1,16 @@
-use std::fmt::{Display, Formatter, Result};
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
 use std::path::Path;
 
-fn parse_input(path: &Path) {
+fn parse_input(
+    path: &Path,
+) -> std::iter::Map<Lines<BufReader<File>>, impl FnMut(Result<String, std::io::Error>) -> String> {
+    let file: File = File::open(path).unwrap(); // open the file
+    let lines: Lines<BufReader<File>> = BufReader::new(file).lines(); // iterator to the reader of the lines of the file
+    lines.map(|line| -> String { line.unwrap().replace("L", "-").replace("R", "") })
+}
+
+fn main() {
     // open the file
     // read line into buffer
     // replace L with -1 or R with nothing
@@ -11,12 +18,7 @@ fn parse_input(path: &Path) {
     // add to accumulator
     // if accumulator is zero, incrememnt counter
     // repeat
-    let file: File = File::open(path).unwrap();
-    BufReader::new(file).lines();
-    return;
-}
-
-fn main() {
     let path: &Path = Path::new("input_test.txt");
-    parse_input(path);
+    let lines = parse_input(path);
+    lines.for_each(|line| println!("Line: {}", line));
 }
