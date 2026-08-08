74fa254a4a85bf92fdd3cff2d9741d6a2a604743
Natalie Chapman <massivephonon.proton.me>
2026-08-08 15:03:32 -0700

    use divisors

--- 2025/d1/src/main.rs
+++ 2025/d1/src/main.rs
@@ -1,6 +1,9 @@
+use std::env::args;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
 use std::path::Path;
+
+const DIALSIZE: i16 = 100;
 
 fn parse_input(path: &Path) -> impl Iterator<Item = i16> {
     let file: File = File::open(path).unwrap(); // open the file
@@ -10,12 +13,11 @@ fn parse_input(path: &Path) -> impl Iterator<Item = i16> {
     rot_strs.map(|rot_str| -> i16 { rot_str.parse::<i16>().unwrap_or_default() })
 }
 
-fn mod100(x: i16) -> i16 {
-    if x < 0 {
-        return 100 - (-x % 100);
-    } else {
-        return x % 100;
-    }
+fn print_dial(dial: i16) {
+    println!(
+        "Dial at {}",
+        (dial % DIALSIZE) + DIALSIZE * i16::from(dial.is_negative())
+    );
 }
 
 fn main() {
@@ -23,20 +25,34 @@ fn main() {
     // read line into buffer
     // replace L with -1 or R with nothing
     // parse into integer
-    // add to accumulator
-    // if accumulator is zero, incrememnt counter
+    // only work in raw position, never mod
+    // count += abs(div(old_pos + rotation, DIALSIZE) - div(old_pos, DIALSIZE))
     // repeat
-    let path: &Path = Path::new("input_test.txt");
+    let args: Vec<String> = args().collect();
+    let path: &Path = Path::new(&args[1]);
     let roterator = parse_input(path); // iterator over input lines that gives integers
-    let mut dial: i16 = 50;
-    let mut accumulator: i16 = 0;
+    let mut pre_rot: i16 = 0;
+    let mut post_rot: i16 = 50;
+    let mut pre_div: i16 = 0;
+    let mut post_div: i16 = post_rot.div_euclid(DIALSIZE);
+    let mut delta_div: i16 = 0;
+    let mut hits: i16 = 0;
+
     roterator.for_each(|rot| {
-        if dial + rot <= 0 || dial + rot >= 100 {
-            // FIX double counts hits and passes
-            accumulator += 1;
-        }
-        println!("dial: {}, rot: {}, acc: {}", dial, rot, accumulator);
-        dial = mod100(dial + rot);
+        // update dial position
+        pre_rot = post_rot;
+        post_rot += rot;
+        if post_rot.is_negative() {
+            println!("DIAL IS NEGATIVE!")
+        };
+        print_dial(post_rot);
+        // update zero hits
+        pre_div = pre_rot.div_euclid(DIALSIZE);
+        println!("pre_div = {}", pre_div);
+        post_div = post_rot.div_euclid(DIALSIZE);
+        println!("post_div = {}", post_div);
+        delta_div = post_div - pre_div;
+        hits += delta_div.abs();
     });
-    println!("Final dial: {}; Final zero count: {}", dial, accumulator)
+    println!("Final zero count: {}", hits);
 }
