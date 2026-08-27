883f7387b9309efc3e065af617d0a5e31550dca7
Natalie Chapman <massivephonon.proton.me>
2026-08-27 07:59:42 -0700

    starting parsing upper bound

--- 2025/d2/src/main.rs
+++ 2025/d2/src/main.rs
@@ -1,23 +1,44 @@
 use std::env::args;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Split};
+use std::io::{BufRead, BufReader, Cursor, Seek};
 use std::path::Path;
 
-fn parse_input(path: &Path) -> Split<BufReader<File>> {
+fn parse_input(path: &Path) {
     let file: File = File::open(path).unwrap(); // open the file
-    let reader: BufReader<File> = BufReader::new(file);
-    let rangestrerator: Split<BufReader<File>> = reader.split(b',');
-    rangestrerator
+    let mut reader: BufReader<File> = BufReader::new(file);
+
+    // read a single range into buffer at a time
+    let mut rangebuff: Vec<u8> = vec![];
+    let _ = reader.read_until(b',', &mut rangebuff);
+
+    let mut cursor: Cursor<Vec<u8>> = Cursor::new(rangebuff);
+    let mut boundbuff: Vec<u8> = vec![];
+
+    // then read lower bound into buffer
+    let lower = cursor.read_until(b'-', &mut boundbuff);
+    let mut bound: u128 = String::from_utf8(boundbuff.split_last().unwrap().1.to_vec())
+        .unwrap()
+        .parse::<u128>()
+        .unwrap();
+    println!("{}", bound);
+
+    // move cursor head to the first digit of upper bound
+    let _ = cursor
+        .seek_relative(i64::from(lower.unwrap()))
+        .unwrap()
+        .read_until(b',', &mut boundbuff);
+    // bound =
+    println!(
+        "{}",
+        String::from_utf8(boundbuff.split_last().unwrap().1.to_vec()).unwrap()
+    );
+    // .parse::<u128>()
+    // .unwrap();
+    // println!("{}", bound)
 }
 
 fn main() {
     let args: Vec<String> = args().collect();
     let path: &Path = Path::new(&args[1]);
-    let rangestrerator = parse_input(path); // iterator over input lines that gives integers
-    for rangestr in rangestrerator {
-        let bytev: Vec<u8> = rangestr.unwrap();
-        let str: String = String::from_utf8(bytev).unwrap();
-        let mut range = str.split('-');
-        println!("{} - {}", range.next().unwrap(), range.next().unwrap());
-    }
+    parse_input(path); // iterator over input lines that gives integers
 }
