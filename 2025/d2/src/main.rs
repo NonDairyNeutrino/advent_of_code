8ee96483d512deeafa33c742f26da9c0ba955969
Natalie Chapman <massivephonon.proton.me>
2026-08-29 10:12:01 -0700

    parse range, lower, and upper bound

--- 2025/d2/src/main.rs
+++ 2025/d2/src/main.rs
@@ -1,6 +1,6 @@
 use std::env::args;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Cursor, Seek};
+use std::io::{BufRead, BufReader, Cursor};
 use std::path::Path;
 
 fn parse_input(path: &Path) {
@@ -8,33 +8,30 @@ fn parse_input(path: &Path) {
     let mut reader: BufReader<File> = BufReader::new(file);
 
     // read a single range into buffer at a time
-    let mut rangebuff: Vec<u8> = vec![];
-    let _ = reader.read_until(b',', &mut rangebuff);
+    let mut range_buff: Vec<u8> = vec![];
+    let _ = reader.read_until(b',', &mut range_buff);
+    let mut range_vec: Vec<u8> = range_buff.split_last().unwrap().1.to_vec();
+    let mut range_str: String = String::from_utf8(range_vec).unwrap();
+    println!("range: {}", range_str);
 
-    let mut cursor: Cursor<Vec<u8>> = Cursor::new(rangebuff);
+    // need to make a cursor to read the range buffer
+    let mut cursor: Cursor<Vec<u8>> = Cursor::new(range_buff);
+    // make one buffer that will ... hmmm
     let mut boundbuff: Vec<u8> = vec![];
 
     // then read lower bound into buffer
-    let lower = cursor.read_until(b'-', &mut boundbuff);
-    let mut bound: u128 = String::from_utf8(boundbuff.split_last().unwrap().1.to_vec())
-        .unwrap()
-        .parse::<u128>()
-        .unwrap();
-    println!("{}", bound);
+    let _: Result<usize, std::io::Error> = cursor.read_until(b'-', &mut boundbuff);
+    let mut bound_vec: Vec<u8> = boundbuff.split_last().unwrap().1.to_vec();
+    let mut bound_str: String = String::from_utf8(bound_vec).unwrap();
+    let mut bound: u128 = bound_str.parse::<u128>().unwrap();
+    println!("lower bound: {}", bound);
 
-    // move cursor head to the first digit of upper bound
-    let _ = cursor
-        .seek_relative(i64::from(lower.unwrap()))
-        .unwrap()
-        .read_until(b',', &mut boundbuff);
-    // bound =
-    println!(
-        "{}",
-        String::from_utf8(boundbuff.split_last().unwrap().1.to_vec()).unwrap()
-    );
-    // .parse::<u128>()
-    // .unwrap();
-    // println!("{}", bound)
+    boundbuff.clear(); // clear buffer to effectively replace string instead of append it
+    let _ = cursor.read_until(b',', &mut boundbuff);
+    bound_vec = boundbuff.split_last().unwrap().1.to_vec();
+    bound_str = String::from_utf8(bound_vec).unwrap();
+    bound = bound_str.parse::<u128>().unwrap();
+    println!("upper bound: {}", bound);
 }
 
 fn main() {
