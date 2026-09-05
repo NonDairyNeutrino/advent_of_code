71b57adf52c11494368bea8fb44c62981d71b1ee
Natalie Chapman <massivephonon.proton.me>
2026-09-05 12:13:27 -0700

    well, it compiles?

--- 2025/d3/src/main.rs
+++ 2025/d3/src/main.rs
@@ -3,28 +3,35 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Lines, Result};
 use std::path::Path;
 
-fn find_max(v: Vec<char>) -> (usize, char) {
-    let max_ref: &char = v.iter().max().unwrap();
+// find the index and value of the rightmost largest number
+fn find_max(v: &Vec<char>) -> (usize, char) {
+    let max: char = *(v.iter().max().unwrap());
     let mut max_ind: usize = v.len() + 1;
     for i in (0..v.len()).rev() {
-        if v[i] == *max_ref {
+        if v[i] == max {
             max_ind = i;
         }
     }
-    return (max_ind, *max_ref);
+    return (max_ind, max);
 }
 
-fn kernel(charv: Vec<char>) -> String {
-    let mut joltage: Vec<char> = Vec::new();
+fn kernel(charv: Vec<char>, joltage: &mut Vec<char>) {
+    let (max_ind, max): (usize, char) = find_max(&charv);
+    if charv.is_empty() {
+        joltage.push(max);
+        kernel(charv[0..max_ind].to_vec(), joltage);
+    }
 }
 
-fn parse_input(path: &Path) -> impl Iterator<Item = ()> {
+fn parse_input(path: &Path) -> impl Iterator<Item = Vec<char>> {
     let file: File = File::open(path).unwrap(); // open the file
     let reader: BufReader<File> = BufReader::new(file);
     let lineterator: Lines<BufReader<File>> = reader.lines();
-    let mut joltagerator = lineterator.map(|line: Result<String>| {
+    let joltagerator = lineterator.map(|line: Result<String>| {
         let charv: Vec<_> = line.unwrap().chars().collect::<Vec<_>>();
-        kernel(charv)
+        let mut joltage: Vec<char> = Vec::new();
+        kernel(charv, &mut joltage);
+        return joltage;
     });
     return joltagerator;
 }
@@ -32,6 +39,6 @@ fn parse_input(path: &Path) -> impl Iterator<Item = ()> {
 fn main() {
     let args: Vec<String> = args().collect();
     let path: &Path = Path::new(&args[1]);
-    let joltagerator = parse_input(path);
+    let mut joltagerator = parse_input(path);
     println!("{:?}", joltagerator.next().unwrap())
 }
