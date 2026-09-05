b302a3acdb8a1a913559a9a137459e46d47cecad
Natalie Chapman <massivephonon.proton.me>
2026-09-05 10:48:39 -0700

    kernelize

--- 2025/d3/src/main.rs
+++ 2025/d3/src/main.rs
@@ -3,15 +3,35 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Lines, Result};
 use std::path::Path;
 
-fn parse_input(path: &Path) {
+fn find_max(v: Vec<char>) -> (usize, char) {
+    let max_ref: &char = v.iter().max().unwrap();
+    let mut max_ind: usize = v.len() + 1;
+    for i in (0..v.len()).rev() {
+        if v[i] == *max_ref {
+            max_ind = i;
+        }
+    }
+    return (max_ind, *max_ref);
+}
+
+fn kernel(charv: Vec<char>) -> String {
+    let mut joltage: Vec<char> = Vec::new();
+}
+
+fn parse_input(path: &Path) -> impl Iterator<Item = ()> {
     let file: File = File::open(path).unwrap(); // open the file
     let reader: BufReader<File> = BufReader::new(file);
     let lineterator: Lines<BufReader<File>> = reader.lines();
-    let bank = lineterator.map(|line: Result<String>| line.unwrap().chars());
+    let mut joltagerator = lineterator.map(|line: Result<String>| {
+        let charv: Vec<_> = line.unwrap().chars().collect::<Vec<_>>();
+        kernel(charv)
+    });
+    return joltagerator;
 }
 
 fn main() {
     let args: Vec<String> = args().collect();
     let path: &Path = Path::new(&args[1]);
-    parse_input(path);
+    let joltagerator = parse_input(path);
+    println!("{:?}", joltagerator.next().unwrap())
 }
